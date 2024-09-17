a08d14d33a52d43d9690b30053d1d810228da2a6
Matthew Borden <matthew.b@buildkite.com>
2024-09-18 08:39:40 +1000

    WIP

--- .buildkite/commands/build-for-testing.sh
+++ .buildkite/commands/build-for-testing.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eu
 APP=${1:-}
 
-export BUILDKITE_AGENT_CACHE_PATHS="vendor/bundle,~/Library/Caches/CocoaPods/,~/.cocoapods,$HOME/Library/Caches/org.swift.swiftpm"
-export BUILDKITE_CACHE_MOUNT_PATH=".cache/buildkite"
+export BUILDKITE_CACHE_MOUNT_PATH="$NSC_CACHE_PATH"
 .buildkite/commands/cache.sh restore "rubygems-{{ checksum \"Gemfile.lock\" }}-podfile-{{ checksum \"Podfile.lock\" }}"
 
 # Run this at the start to fail early if value not available
